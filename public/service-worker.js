d58a05cf497dcae2c7473c0f2189ef46ff2f4f6c
kate <katy.madden99@gmail.com>
2021-04-29 08:36:23 +0100

    added handler for old cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,7 +20,29 @@ const FILES_TO_CACHE = [
     );
   });
   
- 
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          // return array of cache names that are old to delete
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+        })
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+
   
    
   
