38f11f3f332a033e2f197b6a1d7dfea5d853312c
kate <katy.madden99@gmail.com>
2021-04-29 08:35:48 +0100

    added cache install

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,4 +7,20 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png"
   ];
   
+  
+  const STATIC_CACHE = "static-cache-v1";
+  const RUNTIME_CACHE = "runtime-cache";
+  
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
  
+  
+   
+  
